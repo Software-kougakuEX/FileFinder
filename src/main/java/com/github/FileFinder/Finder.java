2f21e5110296fb0fd938c2b9f888ea59badbc1ea
Taga Hayato <g1344766@cse.kyoto-su.ac.jp>
2015-06-28 23:51:10 +0900

    #25 コマンドラインオプションで size オプションが指定された場合の処理を追加する
    コマンドラインオプションで size オプションが指定された場合の処理を追加した。

--- src/main/java/com/github/FileFinder/Finder.java
+++ src/main/java/com/github/FileFinder/Finder.java
@@ -39,8 +39,18 @@ public class Finder {
         if(args.getType() != null){
             flag &= checkTargetType(file, args.getType());
         }
+        if(args.getSize() != null){
+            flag &= checkTargetSize(file, args.getSize());
+        }
+
         return flag;
     }
+    
+    private boolean checkTargetName(File file, String pattern){
+        String name = file.getName();
+        return name.indexOf(pattern) >= 0;
+    }
+    
     private boolean checkTargetType(File file, String type){
         type = type.toLowerCase();
         if(type.equals("d") || type.equals("directory")){
@@ -51,6 +61,27 @@ public class Finder {
         }
         else if(type.equals("h") || type.equals("hidden")){
             return file.isHidden();
+        }
+        return false;
+    }
+
+
+    private boolean checkTargetSize(File file, String sizeString){
+        if(file.isFile()){
+            char sign = sizeString.charAt(0);
+            String string = sizeString.substring(1);
+            int size = Integer.parseInt(string);
+
+            switch(sign){
+            case '>':
+                return file.length() > size;
+            case '<':
+                return file.length() < size;
+            case '=':
+                return file.length() == size;
+            default:
+                // ignore
+            }
         }
         return false;
     }
